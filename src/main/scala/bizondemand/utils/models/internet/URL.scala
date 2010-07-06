44707f5a9608127455443a729e5730b0ebd92e39
Jim Barrows <jim.barrows@bizondemand.biz>
2010-07-06 16:22:28 -0700

    Protocol is now a type.

--- src/main/scala/bizondemand/utils/models/internet/URL.scala
+++ src/main/scala/bizondemand/utils/models/internet/URL.scala
@@ -15,13 +15,37 @@ import java.net.{URL => JavaUrl}
 case class Parameter(key: String, value: String) {
   override def toString = URLEncoder.encode(key) + "=" + URLEncoder.encode(value)
 }
-case class URL(protocol: String,
+
+
+
+class Protocol(protocol:String) {
+	override def toString = protocol
+}
+
+object Protocol {
+	def apply(protocol:String) :Protocol = {
+		protocol.toUpperCase match {
+			case "HTTP" => Http()
+			case "HTTPS" => Https()
+			case "FTP" => Ftp()
+			case "MAIL" =>Mail()
+			case other => new Protocol(other)
+		}
+	}
+}
+
+case class Http() extends Protocol("http") {}
+case class Https() extends Protocol ("https") {}
+case class Ftp() extends Protocol("ftp") {}
+case class Mail() extends Protocol("mail"){}
+
+case class Url(protocol: Protocol,
         username: Option[String],
         password: Option[String],
         domainName: DomainName,
         port: Option[Int], 
         path: Option[List[String]], 
-        parameters: Option[List[Parameter]]) {
+        parameters: Option[List[Parameter]]) {    
         	
   	def +&(param: Parameter) = addParam(param)
 
@@ -33,21 +57,21 @@ case class URL(protocol: String,
 
 	def +<(name: String) = prependDomainName(name)
 
-	def addParam(param: Parameter): URL = new URL(protocol, username, password, domainName, port, path, Some(param :: parameters.getOrElse(Nil)))
+	def addParam(param: Parameter): Url = new Url(protocol, username, password, domainName, port, path, Some(param :: parameters.getOrElse(Nil)))
 
-  	def addAllParams(paramList: List[Parameter]): URL = paramList match {
-    	case Nil => new URL(protocol, username, password, domainName, port, path, parameters)
-    	case _ => new URL(protocol, username, password, domainName, port, path, Some(parameters.getOrElse(Nil) ::: paramList))
+  	def addAllParams(paramList: List[Parameter]): Url = paramList match {
+    	case Nil => new Url(protocol, username, password, domainName, port, path, parameters)
+    	case _ => new Url(protocol, username, password, domainName, port, path, Some(parameters.getOrElse(Nil) ::: paramList))
  	}
 
-  	def addPath(pathPart: String): URL = new URL(protocol, username, password, domainName, port, Some(path.getOrElse(Nil) ::: pathPart :: Nil), parameters)
+  	def addPath(pathPart: String): Url = new Url(protocol, username, password, domainName, port, Some(path.getOrElse(Nil) ::: pathPart :: Nil), parameters)
 
   	def addAllPathParts(pathPartsList: List[String]) = pathPartsList match {
-    	case Nil => new URL(protocol, username, password, domainName, port, path, parameters)
-    	case _ => new URL(protocol, username, password, domainName, port, Some(path.getOrElse(Nil) ::: pathPartsList), parameters)
+    	case Nil => new Url(protocol, username, password, domainName, port, path, parameters)
+    	case _ => new Url(protocol, username, password, domainName, port, Some(path.getOrElse(Nil) ::: pathPartsList), parameters)
   	}
 
-  	def prependDomainName(name: String): URL = new URL(protocol, username, password, DomainName( name :: domainName.subdomains), port, path, parameters)
+  	def prependDomainName(name: String): Url = new Url(protocol, username, password, DomainName( name :: domainName.subdomains), port, path, parameters)
 
   	override def toString = protocol + "://" + username.getOrElse("") + (if (password.isDefined) ":" else "") + password.getOrElse("") + (if (username.isDefined) "@" else "") +
           domainName +
@@ -56,8 +80,8 @@ case class URL(protocol: String,
           parameters.map(_.mkString("?", "&", "")).getOrElse("")
 }
 
-object URL extends Log{
-	def apply(url: String) :URL = {
+object Url extends Log{
+	def apply(url: String) :Url = {
     	val parsed: JavaUrl = new JavaUrl(url)
     
     	var username :Option[String]= None
@@ -70,34 +94,48 @@ object URL extends Log{
       		}
     	}
 
-    	new URL(parsed.getProtocol,
+    	Url( Protocol(parsed.getProtocol),
       		username,
       		password,
       		DomainName.run(parsed.getHost).getOrElse(DomainName(""::Nil)),
-    	if( parsed.getPort < 0) None else Some(parsed.getPort),
-      	if (parsed.getPath == null || parsed.getPath.isEmpty) None else Some(parsed.getPath.split("/").toList.drop(1)),
-      	if (parsed.getQuery == null || parsed.getQuery.isEmpty)
-        	None
-      	else Some(parsed.getQuery.split("&").toList.map(param => {
-        		var pair = param.split("=")
-        		Parameter(pair(0), pair(1))
-      		}))
+    		if( parsed.getPort < 0) None else Some(parsed.getPort),
+      		if (parsed.getPath == null || parsed.getPath.isEmpty) None else Some(parsed.getPath.split("/").toList.drop(1)),
+      		if (parsed.getQuery == null || parsed.getQuery.isEmpty)
+        		None
+      		else 
+      			Some(parsed.getQuery.split("&").toList.map(param => {
+        				var pair = param.split("=")
+        				Parameter(pair(0), pair(1))
+      				})
+      			)
       	)
 	}
 	
-	def apply(protocol:String, domainName:DomainName, path:List[String], parameters:List[Parameter]) :URL = {
-		URL( protocol, None, None, domainName, None, Some(path), Some(parameters))
+	def apply(protocol:String, domainName:DomainName, path:List[String], parameters:List[Parameter]) :Url = {
+		Url( new Protocol(protocol), None, None, domainName, None, Some(path), Some(parameters))
 	}
-	
-	def apply(protocol:String, domainName:DomainName, path:List[String]) :URL = {
-		URL( protocol, None, None, domainName, None, Some(path), None)
+		
+	def apply(protocol:String, domainName:DomainName, path:List[String]) :Url = {
+		Url( new Protocol(protocol), None, None, domainName, None, Some(path), None)
 	}
-	
-	def apply(protocol:String, domainName:DomainName) :URL = {
-		URL( protocol, None, None, domainName, None, None, None)
+			
+	def apply(protocol:String, domainName:DomainName) :Url = {
+		Url( new Protocol(protocol), None, None, domainName, None, None, None)
+	}
+		
+	def apply(protocol:Protocol, domainName:DomainName, path:List[String], parameters:List[Parameter]) :Url = {
+		Url( protocol, None, None, domainName, None, Some(path), Some(parameters))
+	}
+			
+	def apply(protocol:Protocol, domainName:DomainName, path:List[String]) :Url = {
+		Url( protocol, None, None, domainName, None, Some(path), None)
+	}
+			
+	def apply(protocol:Protocol, domainName:DomainName) :Url = {
+		Url( protocol, None, None, domainName, None, None, None)
 	}
 }
 
-object LocalHost80 extends URL("http", None, None, DomainName("localhost"::Nil), None, None, None)
+object LocalHost80 extends Url(Http(), None, None, DomainName("localhost"::Nil), None, None, None) {}
 
-object LocalHost8080 extends URL("http", None, None, DomainName("localhost"::Nil), Some(8080), None, None)
+object LocalHost8080 extends Url(Http(), None, None, DomainName("localhost"::Nil), Some(8080), None, None)
--- src/test/scala/UrlSpec.scala
+++ src/test/scala/UrlSpec.scala
@@ -1,7 +1,8 @@
 package test
 
 import org.specs.Specification
-import bizondemand.utils.models.internet.{Parameter, URL, DomainName}
+import bizondemand.utils.models.internet.{Parameter, Url, DomainName, Http}
+import bizondemand.utils.models.internet.Protocol._
 
 
 class UrlSpecs extends Specification {
@@ -12,20 +13,21 @@ class UrlSpecs extends Specification {
 	
  	"An url class" should {
 		"represent http://localhost" in {
-			new URL("http", None, None, localhost, None, None, None).toString must beEqualTo("http://localhost")
+			val url = Url(Http(), None, None, localhost, None, None, None)
+			 	url.toString must beEqualTo("http://localhost")
     	}
 
     	"represent http://localhost:8080" in {
-      		new URL("http", None, None, localhost, Some(8080), None, None).toString must beEqualTo("http://localhost:8080")
+      		Url(Http(), None, None, localhost, Some(8080), None, None).toString must beEqualTo("http://localhost:8080")
     		}
 
     	"represent http://localhost/some/path/to/some/resource.html" in {
-      		new URL("http", None, None, localhost, None, Some("some" :: "path" :: "to" :: "some" :: "resource.html" :: Nil), None)
+      		Url(Http(), None, None, localhost, None, Some("some" :: "path" :: "to" :: "some" :: "resource.html" :: Nil), None)
       			.toString must beEqualTo("http://localhost/some/path/to/some/resource.html")
     	}
 
     "represent http://localhost/some/path/to/some/resource.html?param1=val1" in {
-		new URL("http", None, None,
+		Url(Http(), None, None,
     		localhost,
         	None,
         	Some("some" :: "path" :: "to" :: "some" :: "resource.html" :: Nil),
@@ -33,7 +35,7 @@ class UrlSpecs extends Specification {
     }
     
     "represent http://localhost/some/path/to/some/resource.html?param1=val1&param2=val2" in {
-    	new URL("http",
+    	Url(Http(),
     		None, None,
     		localhost,
     		None,
@@ -43,27 +45,27 @@ class UrlSpecs extends Specification {
     }
 
     "be able to a param after creation" in {
-    	val original = new URL("http", None, None, localhost, None, None, None)
+    	val original = Url(Http(), None, None, localhost, None, None, None)
     	val modified = original.addParam(Parameter("key1", "val1"))
     	modified.toString must beEqualTo("http://localhost?key1=val1")
     	modified.toString must beDifferent(original.toString)
     }
 
     "be able to add an empty list of params to an empty list of params" in {
-    	val original = new URL("http", None, None, localhost, None, None, None)
+    	val original = Url(Http(), None, None, localhost, None, None, None)
     	val modified = original.addAllParams(Nil)
     	modified.toString must beEqualTo("http://localhost")
     }
 
     "be able to add a list of params to an empty List of params" in {
-    	val original = new URL("http", None, None, localhost, None, None, None)
+    	val original = Url(Http(), None, None, localhost, None, None, None)
     	val modified = original.addAllParams(Parameter("key1", "val1") :: Parameter("key2", "val2") :: Nil)
     	modified.toString must beEqualTo("http://localhost?key1=val1&key2=val2")
     	modified.toString must beDifferent(original.toString)
     }
 
     "be able to add a list of params to a List of params" in {
-    	val original = new URL("http", None, None, localhost, None, None, Some(Parameter("key1", "val1") :: Parameter("key2", "val2") :: Nil))
+    	val original = Url(Http(), None, None, localhost, None, None, Some(Parameter("key1", "val1") :: Parameter("key2", "val2") :: Nil))
     	val modified = original.++&(Parameter("key3", "val3") :: Parameter("key4", "val4") :: Nil)
     	modified.toString must beEqualTo("http://localhost?key1=val1&key2=val2&key3=val3&key4=val4")
     	modified.toString must beDifferent(original.toString)
@@ -71,50 +73,50 @@ class UrlSpecs extends Specification {
 
 
     "be able to a path after creation" in {
-    	val original = new URL("http", None, None, localhost, None, None, None)
+    	val original = Url(Http(), None, None, localhost, None, None, None)
     	val modified = original.addPath("dir1")
     	modified.toString must beEqualTo("http://localhost/dir1")
     	modified.toString must beDifferent(original.toString)
     }
 
     "be able to add an empty list of paths to an empty list of paths" in {
-     	val original = new URL("http", None, None, localhost, None, None, None)
+     	val original = Url(Http(), None, None, localhost, None, None, None)
     	val modified = original.addAllPathParts(Nil)
     	modified.toString must beEqualTo("http://localhost")
     }
 
     "be able to add a list of paths to an empty List of paths" in {
-    	val original = new URL("http", None, None, localhost, None, None, None)
+    	val original = Url(Http(), None, None, localhost, None, None, None)
     	val modified = original.addAllPathParts("dir1" :: "dir2" :: Nil)
     	modified.toString must beEqualTo("http://localhost/dir1/dir2")
     	modified.toString must beDifferent(original.toString)
     }
 
     "be able to add a list of paths to a List of paths" in {
-      val original = new URL("http", None, None, localhost, None, Some("dir1" :: "dir2" :: Nil), None)
+      val original = Url(Http(), None, None, localhost, None, Some("dir1" :: "dir2" :: Nil), None)
       val modified = original.++/("dir3" :: "dir4" :: Nil)
       modified.toString must beEqualTo("http://localhost/dir1/dir2/dir3/dir4")
       modified.toString must beDifferent(original.toString)
     }
 
     "be able to render http://username@domain" in {
-      new URL("http", Some("username"), None, localhost, None, None, None).toString must beEqualTo("http://username@localhost")
+      Url(Http(), Some("username"), None, localhost, None, None, None).toString must beEqualTo("http://username@localhost")
     }
 
     "be able to render http://username:password@domain" in {
-      new URL("http", Some("username"), Some("password"), localhost, None, None, None).toString must beEqualTo("http://username:password@localhost")
+      Url(Http(), Some("username"), Some("password"), localhost, None, None, None).toString must beEqualTo("http://username:password@localhost")
     }
 
-    "be able to prepend a name to a URL " in {
-      val original = new URL("http", Some("username"), Some("password"), localdomain, None, None, None)
+    "be able to prepend a name to a Url " in {
+      val original = Url(Http(), Some("username"), Some("password"), localdomain, None, None, None)
       val modified = original +< ("localhost")
       modified.toString must beEqualTo("http://username:password@localhost.localdomain")
       modified.toString must beDifferent(original.toString)
     }
 
     "be able to parse http://username:password@localhost.localdomain:80/my/path/to/here.xml?key1=param1&key2=param2" in {
-      val actual = URL("http://username:password@localhost.localdomain:80/my/path/to/here.xml?key1=param1&key2=param2")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost.localdomain:80/my/path/to/here.xml?key1=param1&key2=param2")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhostLocaldomain,
@@ -125,8 +127,8 @@ class UrlSpecs extends Specification {
     }
 
      "be able to parse http://username:password@localhost.localdomain:80/my/path/to/here.xml?key1=param1" in {
-      val actual = URL("http://username:password@localhost.localdomain:80/my/path/to/here.xml?key1=param1")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost.localdomain:80/my/path/to/here.xml?key1=param1")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhostLocaldomain,
@@ -137,8 +139,8 @@ class UrlSpecs extends Specification {
     }
 
      "be able to parse http://username:password@localhost.localdomain:80/my/path/to/here.xml" in {
-      val actual = URL("http://username:password@localhost.localdomain:80/my/path/to/here.xml")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost.localdomain:80/my/path/to/here.xml")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhostLocaldomain,
@@ -149,8 +151,8 @@ class UrlSpecs extends Specification {
     }
 
      "be able to parse http://username:password@localhost.localdomain:80/my" in {
-      val actual = URL("http://username:password@localhost.localdomain:80/my")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost.localdomain:80/my")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhostLocaldomain,
@@ -161,8 +163,8 @@ class UrlSpecs extends Specification {
     }
 
      "be able to parse http://username:password@localhost.localdomain:80" in {
-      val actual = URL("http://username:password@localhost.localdomain:80")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost.localdomain:80")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhostLocaldomain,
@@ -173,8 +175,8 @@ class UrlSpecs extends Specification {
     }
 
     "be able to parse http://username:password@localhost.localdomain" in {
-      val actual = URL("http://username:password@localhost.localdomain")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost.localdomain")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhostLocaldomain,
@@ -185,8 +187,8 @@ class UrlSpecs extends Specification {
     }
 
     "be able to parse http://username:password@localhost" in {
-      val actual = URL("http://username:password@localhost")
-      val expected = new URL("http",
+      val actual = Url("http://username:password@localhost")
+      val expected = new Url(Http(),
         Some("username"),
         Some("password"),
         localhost,
@@ -197,8 +199,8 @@ class UrlSpecs extends Specification {
     }
 
     "be able to parse http://username@localhost" in {
-      val actual = URL("http://username@localhost")
-      val expected = new URL("http",
+      val actual = Url("http://username@localhost")
+      val expected = new Url(Http(),
         Some("username"),
         None,
         localhost,
@@ -209,8 +211,8 @@ class UrlSpecs extends Specification {
     }
 
     "be able to parse http://localhost" in {
-      val actual = URL("http://localhost")
-      val expected = new URL("http",
+      val actual = Url("http://localhost")
+      val expected = new Url(Http(),
         None,
         None,
         localhost,
@@ -220,9 +222,9 @@ class UrlSpecs extends Specification {
       actual.toString must be_== (expected.toString)
     }
     
-    "be able to determine if 2 URL's are the same" in {
-   		val actual = URL("http://localhost")
-     	val expected = new URL("http",
+    "be able to determine if 2 Url's are the same" in {
+   		val actual = Url("http://localhost")
+     	val expected = new Url(Http(),
         	None,
 	        None,
     	    localhost,
@@ -233,8 +235,8 @@ class UrlSpecs extends Specification {
     }
     
     "be able to accept protocol, domainName, path and parameters only" in {
-    	val expected = URL("http://localhost/path/to/something?param1=val1")
-     	val actual = URL("http",        	
+    	val expected = Url("http://localhost/path/to/something?param1=val1")
+     	val actual = Url(Http(),        	
     	    localhost,
         	"path"::"to"::"something"::Nil,	        
     	    Parameter("param1", "val1")::Nil
@@ -243,8 +245,8 @@ class UrlSpecs extends Specification {
     }
     
     "be able to accept protocol, domainName, and path  only" in {
-    	val expected = URL("http://localhost/path/to/something")
-     	val actual = URL("http",        	
+    	val expected = Url("http://localhost/path/to/something")
+     	val actual = Url(Http(),        	
     	    localhost,
         	"path"::"to"::"something"::Nil
     	    ) 	
@@ -252,8 +254,8 @@ class UrlSpecs extends Specification {
     }
     
      "be able to accept protocol, domainName, and path  only" in {
-    	val expected = URL("http://localhost")
-     	val actual = URL("http",        	
+    	val expected = Url("http://localhost")
+     	val actual = Url(Http(),        	
     	    localhost
     	    ) 	
     	actual must be_==( expected)
