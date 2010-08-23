0f07745c3a12bca0b15eef8bb592b4d63d3f54ea
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-08-22 22:01:21 -0700

    Updated to push to artifvactory

--- project/build/UtilsProject.scala
+++ project/build/UtilsProject.scala
@@ -3,13 +3,22 @@ import sbt._
 class UtilsProject(info: ProjectInfo) extends DefaultProject(info)
 {
   
-	val maven = DefaultMavenRepository
-	val scalaToolsRelease = ScalaToolsReleases
-	val scalaToolsSnapshots = ScalaToolsSnapshots
-	val javaNet = JavaNet1Repository	
+	 val bizOnDemand_libs_Releases     =  "libs-releases-local"   at 
+			"http://bizondemand.biz:8081/artifactory/libs-releases-local"
+  val bizOnDemand_libs_Snapshots    = "libs-snapshots-local"   at 
+			"http://bizondemand.biz:8081/artifactory/libs-snapshots-local"
+  val bizOnDemand_plugins_Releases  = "plugins-releases-local" at 
+			"http://bizondemand.biz:8081/artifactory/plugins-releases-local"
+  val bizOnDemand_plugins_Snapshots = "plugins-snaphots-local" at 
+			"http://bizondemand.biz:8081/artifactory/plugins-snapshots-local"
+  
+	val scalatoolsSnapshot = 
+    "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
+
+	override def managedStyle=ManagedStyle.Maven
+	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  val publishTo = "Biz On Demand Artifacts" at "http://bizondemand.biz:8081/artifactory/libs-releases-local"
 	
-	val bizOnDemand = "BizOnDemand-release" at "http://bizondemand.biz/artifacts"
-		
 			
 	val jetty = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
 	val junit = "junit" % "junit" % "4.5" % "test->default"	
@@ -21,7 +30,4 @@ class UtilsProject(info: ProjectInfo) extends DefaultProject(info)
 		case "2.8.0" => "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default"
 	}
   	
-	override def managedStyle = ManagedStyle.Maven
-  	val publishTo = Resolver.ssh("BizOnDemand", "bizondemand.biz", "/var/local/artifacts/release/")  	
-
 }
